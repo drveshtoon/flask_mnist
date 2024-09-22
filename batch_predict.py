fc5841e3fc090b47662c9d7c72ff05b423764851
drveshtoon <140492210+drveshtoon@users.noreply.github.com>
2024-09-22 21:00:06 +0500

    Update batch_predict.py

--- batch_predict.py
+++ batch_predict.py
@@ -9,11 +9,23 @@ image_dir = 'new_data'
 image_paths = [os.path.join(image_dir, filename) for filename in os.listdir(image_dir) if filename.endswith('.jpg')]
 
 if image_paths:
-    response = requests.post(api_url, json={'image_paths': image_paths})
-    predictions = response.json().get('predictions', [])
+    files = []
+    # Open each image file and append it to the 'files' list
+    for image_path in image_paths:
+        with open(image_path, 'rb') as img:
+            files.append(('images', (os.path.basename(image_path), img, 'image/jpeg')))
+    
+    # Send the images to the Flask API
+    response = requests.post(api_url, files=files)
 
-    # Save or print batch results
-    for prediction in predictions:
-        print(prediction)
+    # Parse the response from the API
+    if response.status_code == 200:
+        predictions = response.json().get('predictions', [])
+        
+        # Save or print batch results
+        for prediction in predictions:
+            print(prediction)
+    else:
+        print(f"Error: Received status code {response.status_code}")
 else:
     print('No images found in new_data directory.')
